d348b69481ecb68590cc13df1440697c8e5a4e88
Brendan Schiller <schiller.brendan@gmail.com>
2019-03-26 15:36:57 -0600

    completed

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios
+      .get(
+        `http://localhost:3005/students?class=${this.props.match.params.class}`
+      )
+      .then(res => {
+        this.setState({
+          students: res.data
+        })
+      })
   }
 
   render() {
+    const students = this.state.students.map((item, i, arr) => {
+      return (
+        <div key={i}>
+          <Link to={`/student/${item.id}`}>
+            <h3>
+              {item.first_name} {item.last_name}
+            </h3>
+          </Link>
+        </div>
+      )
+    })
+
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {students}
       </div>
     )
   }
